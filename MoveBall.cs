920a27242575602facff23146ac9a5af6e0892e1
dimi44tri <ddebarne@fau.edu>
2017-01-09 19:55:26 -0500

    Motion controllers
    scripts to control single ball and each player

---
+++ MoveBall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBall : MonoBehaviour
+{
+    //public float speed;
+    public Vector3 initialImpulse;
+
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //allows manual control in editor for ball movement
+        rb.AddForce(initialImpulse, ForceMode.Impulse);
+    }
+}
+
---
+++ PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//player identifier attribute in editor
+public enum ePlayer {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+[System.Serializable] //<-- Makes the below class show up as a new property in the unity inspector
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;    
+    public ePlayer Player;
+
+    void FixedUpdate()
+    {
+        float inputDirection = 0; //direction of player input relative to player ID
+        switch (Player)
+        {
+            case ePlayer.Down:
+                //Referencing custom virtual input settings from editor
+                inputDirection = Input.GetAxisRaw("Player1");
+                //Not using physics engine --- time property used to standardize motion timing across different devices
+                transform.position += new Vector3(inputDirection * speed * Time.deltaTime, 0, 0);
+                break;
+
+            case ePlayer.Up:
+                inputDirection = Input.GetAxisRaw("Player2");
+                transform.position += new Vector3(inputDirection * speed * Time.deltaTime, 0, 0);
+                break;
+
+            case ePlayer.Left:
+                inputDirection = Input.GetAxisRaw("Player3");
+                transform.position += new Vector3(0, 0, inputDirection * speed * Time.deltaTime);
+                break;
+
+            default:
+                inputDirection = Input.GetAxisRaw("Player4");
+                transform.position += new Vector3(0, 0, inputDirection * speed * Time.deltaTime);
+                break;
+        }
+
+
+    }
+
+
+
+
+
+
+    
+    ////Rigidbody variable to capture the same-named component from Player sphere object in unity
+    //public float speed;
+    //private Rigidbody rb;
+
+    //void Start()
+    //{
+    //    rb = GetComponent<Rigidbody>();
+
+    //}
+
+    ////Fixed update used applying forces
+    //void FixedUpdate()
+    //{
+    //    float moveHorizontal = Input.GetAxis("Horizontal");
+
+    //    //y is zero since player isn't moving up off the plane
+    //    Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+    //    //apply a force to sphere based on received Input (for kinematic setting)
+    //    rb.AddForce(movement * speed);
+
+    //}
+}
